011bc34dc32c146e1c2c8048112ebab13f4c417b
W08E <w08e@aliyun.com>
2023-12-03 22:11:54 +0800

    feature-启动类，pom和yml调整

--- user-service/src/main/java/com/w08e/xiaomai/application.java
+++ user-service/src/main/java/com/w08e/xiaomai/application.java
@@ -1,10 +1,15 @@
 package com.w08e.xiaomai;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author: w08e
  */
+@SpringBootApplication
 public class application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(application.class, args);
     }
+
 }
